7d584ce178a47b713992e3345b24d99c978fabfd
Dum Dum Goose <liyanpei.jack@gmail.com>
2024-01-07 18:04:24 +0800

    🔨: fixed request url typo

--- artscription-ui/src/pages/api/artscriptionDetail.js
+++ artscription-ui/src/pages/api/artscriptionDetail.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
     const { id } = req.query;
     const host = process.env.REST_HOST || 'http://localhost:3000';
-    const targetUrl = `${host}/getLast50Artscription`;
+    const targetUrl = `${host}/artscriptionDetail?id=${id}`;
 
     try {
         const apiRes = await fetch(targetUrl, {
