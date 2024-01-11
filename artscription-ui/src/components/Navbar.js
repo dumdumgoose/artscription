83bae1ccba9435a15eb5b5582ba6c4f94417e2dc
Dum Dum Goose <liyanpei.jack@gmail.com>
2024-01-11 19:40:21 +0800

    🎉: added link for twitter and tg

--- artscription-ui/src/components/Navbar.js
+++ artscription-ui/src/components/Navbar.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import Link from 'next/link';
+import { FaTwitter, FaTelegram } from 'react-icons/fa';
 
 export default function Navbar() {
 
@@ -21,24 +22,26 @@ export default function Navbar() {
         <div className="fixed w-full top-0 border-b border-gray-200 bg-white z-50">
             <div className="flex flex-col w-full items-center p-0 mx-auto px-4 sm:px-6 lg:px-8 max-w-screen-lg">
                 <nav className="h-20 w-full flex justify-between items-center">
-                    <Link href="/" className="text-lg md:text-xl lg:text-2xl text-slate-500 text-blue-600">ARTSCRIPTION</Link>
-                    {/*<div className="hidden md:flex text-gray-500">*/}
-                    {/*    <Link href="/artscriptions" className={`font-bold hover:text-blue-600 text-sm md:text-base cursor-pointer mr-4 ${activeLink === '/artscriptions' ? 'text-blue-600' : ''}`}>Artscriptions</Link>*/}
-                    {/*    <Link href="/token" className={`font-bold hover:text-blue-600 text-sm md:text-base cursor-pointer mr-4 ${activeLink === '/token' ? 'text-blue-600' : ''}`}>Token</Link>*/}
-                    {/*    <Link href="/marketplace" className={`font-bold hover:text-blue-600 text-sm md:text-base cursor-pointer mr-4 ${activeLink === '/marketplace' ? 'text-blue-600' : ''}`}>Marketplace</Link>*/}
-                    {/*</div>*/}
-                    <ConnectButton />
-                    <button className="md:hidden text-4xl" onClick={() => setIsMenuOpen(!isMenuOpen)}>=</button>
-                </nav>
-                {isMenuOpen && (
-                    <div className="absolute top-full left-0 right-0 bg-white shadow-md py-2 md:hidden">
-                        <Link href="/artscriptions" className={`block py-2 text-center font-bold hover:text-blue-600 text-sm md:text-base cursor-pointer ${activeLink === '/artscriptions' ? 'text-blue-600' : ''}`}>Artscriptions</Link>
-                        <Link href="/token" className={`block py-2 text-center font-bold hover:text-blue-600 text-sm md:text-base cursor-pointer ${activeLink === '/token' ? 'text-blue-600' : ''}`}>Token</Link>
-                        <Link href="/marketplace" className={`block py-2 text-center font-bold hover:text-blue-600 text-sm md:text-base cursor-pointer ${activeLink === '/marketplace' ? 'text-blue-600' : ''}`}>Marketplace</Link>
+                    <Link href="/"
+                          className="text-lg md:text-xl lg:text-2xl text-slate-500">ARTSCRIPTION</Link>
+                    <div className="hidden md:flex text-gray-500">
+                        <Link href="/artscriptions"
+                              className={ `font-bold hover:text-blue-600 text-sm md:text-base cursor-pointer mr-4 ${ activeLink === '/artscriptions' ? 'text-blue-600' : '' }` }>Artscriptions</Link>
+                        <Link href="/token"
+                              className={ `font-bold hover:text-blue-600 text-sm md:text-base cursor-pointer mr-4 ${ activeLink === '/token' ? 'text-blue-600' : '' }` }>Token</Link>
+                        <Link href="/marketplace"
+                              className={ `font-bold hover:text-blue-600 text-sm md:text-base cursor-pointer mr-4 ${ activeLink === '/marketplace' ? 'text-blue-600' : '' }` }>Marketplace</Link>
                     </div>
-                )}
+                    <div className="hidden md:flex text-gray-500">
+                        <Link href="https://twitter.com/Art_inscription" className='link-style'><FaTwitter
+                          className='link-icon'/></Link>
+                        <Link href="https://t.me/artela_official" className='link-style'><FaTelegram
+                          className='link-icon'/></Link>
+                    </div>
+                    <ConnectButton/>
+                </nav>
             </div>
         </div>
-    );
+);
 
 }
--- artscription-ui/src/pages/index.tsx
+++ artscription-ui/src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Navbar from '../components/Navbar';
-import Link from 'next/link';
 
 export default function Home() {
 
@@ -54,7 +53,7 @@ export default function Home() {
             <div className="flex flex-col items-center p-0 md:w-[1200px] m-auto">
                 <div id="firstScreen" className="flex min-h-screen flex-col items-center justify-center px-4">
                     <div
-                        className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+                        className="relative flex flex-col place-items-center text-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
                         <p className="text-[48px] text-blod">Inscription in Artela</p>
                         <p className="text-[24px] pt-10">Support decentralized applications based on the indexer ledger architecture</p>
                     </div>
@@ -116,7 +115,7 @@ export default function Home() {
                         </a>
                     </div>
                     <div className="h-20 w-full py-8 text-2xl flex flex-row justify-center mb-4">
-                        <p className="text-xl text-gray-700">Other Art20 inscriptions are in development.</p>
+                        <p className="text-xl text-gray-700">More to come...</p>
                     </div>
                 </div>
 
@@ -126,8 +125,8 @@ export default function Home() {
                     </div>
                     <div
                         className="h-auto w-full text-center flex flex-col py-8 px-8 mt-4 mb-4 border border-gray-200 rounded-lg">
-                        <p className="text-3xl font-bold text-gray-700 py-4">Startup</p>
-                        <p className="text-xl text-gray-700 font-bold py-2">Startup Artscription at testnet stage in
+                        <p className="text-3xl font-bold text-gray-700 py-4">About Artscription</p>
+                        <p className="text-xl text-gray-700 font-bold py-2">About Artscription at testnet stage in
                             Artela Blockchain</p>
                         <p className="text-xl text-gray-700 py-2">Artscription is the first inscription project in
                             Artela Blockchain, utilizing its unique Aspect programming to realize on-chain indexer and
