af679ef24c9a422024cfb4f5897068bc1a039c94
fnkk <1748501327@qq.com>
2024-02-05 10:06:42 +0800

    refactor: :art: Project Catalog Adjustment

---
+++ artscription-ui/src/app/page.tsx
@@ -0,0 +1,12 @@
+
+
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
+export default function Page() {
+  return (
+    <div>
+      <div className='h-[500px] bg-green-500'>6666</div>
+    </div>
+
+  );
+}
